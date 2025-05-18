f0600f4f6a686a104c74a2e55ab1ed7592525e52
utkmiz13 <Um708179@gmail.com>
2025-05-18 14:08:41 +0530

    Create Helo.cpp

---
+++ Helo.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<(1* -1)/n
+    cout<<(2* -2)/n
+    cout<<(3* -3)/n
+    cout<<(4* -4)/n 
+    cout<<(5*- 5)/n
+
+    return 0;
+}
