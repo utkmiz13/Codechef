7434d8d0bbed2fd31ab0a91f611950efba4234fd
utkmiz13 <Um708179@gmail.com>
2025-05-18 16:40:13 +0530

    Update Helo.cpp

--- Helo.cpp
+++ Helo.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-    cout<<(1* -1)/n
-    cout<<(2* -2)/n
-    cout<<(3* -3)/n
-    cout<<(4* -4)/n 
-    cout<<(5*- 5)/n
+    cout<<(1* -1)<<endl;
+    cout<<(2* -2)<<endl;
+    cout<<(3* -3)<<endl;
+    cout<<(4* -4)<<endl;
+    cout<<(5*- 5)<<endl;
 
     return 0;
 }
